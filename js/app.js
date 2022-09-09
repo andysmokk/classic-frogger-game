80ad73b8ba9c5372a6b1df3ba1ffe020f697df6b
Andrii Konovalov <andy.smokk@gmail.com>
2022-09-09 13:17:57 +0200

    done

--- js/app.js
+++ js/app.js
@@ -1,50 +1,72 @@
 // Enemies our player must avoid
-var Enemy = function (x, y, movement) {
+const INITIAL_POSITION_PLAYER_X = 203;
+const INITIAL_POSITION_ENEMIES_X = -200;
+const INITIAL_POSITION_ENEMY_ONE_Y = 62;
+const INITIAL_POSITION_ENEMY_TWO_Y = 145;
+const INITIAL_POSITION_ENEMY_THREE_Y = 228;
+const INITIAL_POSITION_PLAYER_Y = 390;
+const BLOCK_HEIGHT = 83;
+const BLOCK_WIDTH = 101;
+const MIN_SPEED = 100;
+const MAX_SPEED = 400;
+
+var Enemy = function (x, y) {
   this.x = x;
   this.y = y;
-  this.movement = movement;
-  // Variables applied to each of our instances go here,
-  // we've provided one for you to get started
-
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
+  this.speed = this.randomInteger();
   this.sprite = "images/enemy-bug.png";
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function (dt) {
-  this.x += this.movement * dt;
+  this.x += this.speed * dt;
+
   if (this.x > 500) {
-    this.x = -95;
+    this.x = INITIAL_POSITION_ENEMIES_X;
+    this.speed = this.randomInteger();
   }
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
+
+  if (
+    player.y < this.y + 50 &&
+    player.y + 50 > this.y &&
+    player.x < this.x + 75 &&
+    player.x + 75 > this.x
+  ) {
+    Player.prototype.initialPositionPlayer();
+  }
 };
 
-// Draw the enemy on the screen, required method for game
+Enemy.prototype.randomInteger = function () {
+  return Math.floor(MIN_SPEED + Math.random() * (MAX_SPEED + 1 - MIN_SPEED));
+};
+
 Enemy.prototype.render = function () {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+const enemyOne = new Enemy(
+  INITIAL_POSITION_ENEMIES_X,
+  INITIAL_POSITION_ENEMY_ONE_Y
+);
+const enemyTwo = new Enemy(
+  INITIAL_POSITION_ENEMIES_X,
+  INITIAL_POSITION_ENEMY_TWO_Y
+);
+const enemyThree = new Enemy(
+  INITIAL_POSITION_ENEMIES_X,
+  INITIAL_POSITION_ENEMY_THREE_Y
+);
 
-// Now instantiate your objects.
-let enemyOne = new Enemy(0, 62, 200);
-let enemyTwo = new Enemy(0, 145, 200);
-let enemyThree = new Enemy(0, 228, 200);
-
-// Place all enemy objects in an array called allEnemies
 const allEnemies = [enemyOne, enemyTwo, enemyThree];
-// Place the player object in a variable called player
 
 var Player = function (x, y) {
   this.x = x;
   this.y = y;
   this.sprite = "images/char-boy.png";
+};
+
+Player.prototype.initialPositionPlayer = function () {
+  player.x = INITIAL_POSITION_PLAYER_X;
+  player.y = INITIAL_POSITION_PLAYER_Y;
 };
 
 Player.prototype.update = function (dt) {
@@ -57,28 +79,32 @@ Player.prototype.render = function () {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-let player = new Player(203, 390);
-
 Player.prototype.handleInput = function (keyPressed) {
-  console.log(this.x);
-  console.log(this.y);
-
   if (keyPressed === "up" && this.y > 0) {
-    this.y -= 83;
+    this.y -= BLOCK_HEIGHT;
   }
+
   if (keyPressed === "down" && this.y < 390) {
-    this.y += 83;
+    this.y += BLOCK_HEIGHT;
   }
+
   if (keyPressed === "left" && this.x > 1) {
-    this.x -= 101;
+    this.x -= BLOCK_WIDTH;
   }
+
   if (keyPressed === "right" && this.x < 405) {
-    this.x += 101;
+    this.x += BLOCK_WIDTH;
+  }
+
+  if (this.y < 0) {
+    setTimeout(() => {
+      this.initialPositionPlayer();
+    }, 500);
   }
 };
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+const player = new Player(INITIAL_POSITION_PLAYER_X, INITIAL_POSITION_PLAYER_Y);
+
 document.addEventListener("keyup", function (e) {
   var allowedKeys = {
     37: "left",
